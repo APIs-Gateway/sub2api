newcodebook/xoaai

fix(openai-compat): Responses→Chat 桥接按 reasoning item id 缓存回注 reasoning_content
修复 #5520：Codex 经 force_chat_completions 桥接到 DeepSeek thinking 上游时，
历史中的 encrypted-only reasoning item（summary 为空 + 不透明 encrypted_content，
远程 compaction / 跨会话恢复后常见）取不出明文，后续 assistant 消息缺
reasoning_content，DeepSeek 400 "The reasoning_content in the thinking mode
must be passed back to the API"，客户端仅看到通用 502。

reasoning item 的 id 一定会被客户端回传，以其为 key 做服务端缓存：

- GatewayCache 新增 Set/GetReasoningContent（Redis，默认 TTL 7 天）
- 响应侧：流式扫 response.output_item.done、非流式扫 output，把 reasoning
  全文按 item id 写缓存；客户端断连后 drain 期间用 detached ctx 仍会写完
- 请求侧：apicompat 新增 ResponsesToChatCompletionsRequestWithOptions 与
  ReasoningContentByID 钩子，encrypted-only item 查缓存补回 pendingReasoning；
  缓存 miss/出错一律 fail-open 维持原行为
- 自愈：历史里带明文 summary 的 reasoning item 顺手刷新缓存，覆盖 Redis
  flush / 跨实例漂移

测试：apicompat（命中恢复/miss 保持原样/明文优先）、service 端到端（流式
写缓存、请求侧回注+自愈）、repository miniredis 存取。

backend/internal/service/gateway_hotpath_optimization_test.go
@@ -158,6 +158,13 @@ func (s *stickyGatewayCacheHotpathStub) ReleaseGrokVideoBilled(_ context.Context
 	return nil
 }
 
+func (s *stickyGatewayCacheHotpathStub) SetReasoningContent(_ context.Context, _ string, _ string, _ time.Duration) error {
+	return nil
+}
+func (s *stickyGatewayCacheHotpathStub) GetReasoningContent(_ context.Context, _ string) (string, error) {
+	return "", ErrReasoningContentNotFound
+}
+
 func (s *modelsListAccountRepoStub) ListSchedulableByGroupID(ctx context.Context, groupID int64) ([]Account, error) {
 	s.listByGroupCalls.Add(1)
 	if s.err != nil {

backend/internal/service/gateway_multiplatform_test.go
@@ -291,6 +291,13 @@ func (m *mockGatewayCacheForPlatform) ReleaseGrokVideoBilled(_ context.Context, 
 	return nil
 }
 
+func (m *mockGatewayCacheForPlatform) SetReasoningContent(_ context.Context, _ string, _ string, _ time.Duration) error {
+	return nil
+}
+func (m *mockGatewayCacheForPlatform) GetReasoningContent(_ context.Context, _ string) (string, error) {
+	return "", ErrReasoningContentNotFound
+}
+
 type mockGroupRepoForGateway struct {
 	groups           map[int64]*Group
 	getByIDCalls     int

backend/internal/service/gemini_multiplatform_test.go
@@ -319,6 +319,13 @@ func (m *mockGatewayCacheForGemini) ReleaseGrokVideoBilled(_ context.Context, _ 
 	return nil
 }
 
+func (m *mockGatewayCacheForGemini) SetReasoningContent(_ context.Context, _ string, _ string, _ time.Duration) error {
+	return nil
+}
+func (m *mockGatewayCacheForGemini) GetReasoningContent(_ context.Context, _ string) (string, error) {
+	return "", ErrReasoningContentNotFound
+}
+
 // TestGeminiMessagesCompatService_SelectAccountForModelWithExclusions_GeminiPlatform 测试 Gemini 单平台选择
 func TestGeminiMessagesCompatService_SelectAccountForModelWithExclusions_GeminiPlatform(t *testing.T) {
 	ctx := context.Background()

APIs-Gateway/sub2api

(cherry picked from commit 612436a5a7cbb7ab9d4ce4174fcdff948fab31fb)

backend/internal/service/gateway_hotpath_optimization_test.go
@@ -883,3 +883,11 @@ func TestSelectAccountWithLoadAwareness_StickyReadReuse(t *testing.T) {
 		require.Equal(t, int64(1), cache.getCalls.Load())
 	})
 }
+
+func (c *stickyGatewayCacheHotpathStub) SetReasoningContent(_ context.Context, _ string, _ string, _ time.Duration) error {
+	return nil
+}
+
+func (c *stickyGatewayCacheHotpathStub) GetReasoningContent(_ context.Context, _ string) (string, error) {
+	return "", ErrReasoningContentNotFound
+}

backend/internal/service/gateway_multiplatform_test.go
@@ -3400,3 +3400,11 @@ func TestGatewayService_ResolveGatewayGroup_DetectsFallbackCycle(t *testing.T) {
 	require.Nil(t, gotID)
 	require.Contains(t, err.Error(), "fallback group cycle")
 }
+
+func (c *mockGatewayCacheForPlatform) SetReasoningContent(_ context.Context, _ string, _ string, _ time.Duration) error {
+	return nil
+}
+
+func (c *mockGatewayCacheForPlatform) GetReasoningContent(_ context.Context, _ string) (string, error) {
+	return "", ErrReasoningContentNotFound
+}

backend/internal/service/gemini_multiplatform_test.go
@@ -976,3 +976,11 @@ func TestGeminiMessagesCompatService_isModelSupportedByAccount(t *testing.T) {
 		})
 	}
 }
+
+func (c *mockGatewayCacheForGemini) SetReasoningContent(_ context.Context, _ string, _ string, _ time.Duration) error {
+	return nil
+}
+
+func (c *mockGatewayCacheForGemini) GetReasoningContent(_ context.Context, _ string) (string, error) {
+	return "", ErrReasoningContentNotFound
+}